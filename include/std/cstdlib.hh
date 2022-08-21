c02740faf82e6614006cf9457cbb3160b3267ff2
BlueCannonBall <mmttiizz100@gmail.com>
2022-08-21 19:25:13 -0400

    Add simd

--- include/std/cstdlib.hh
+++ include/std/cstdlib.hh
@@ -1,13 +1,31 @@
 #pragma once
 
 #include <std/cstddef.hh>
+#include <x86intrin.h>
 
 namespace std2::memory
 {   
-    template <typename T>
-    void Move(T *destination, T const *source, size_t size)
+    void Move(void *destination, void const *source, size_t size)
     {
-        for (uintptr_t i = 0; i < size; i++)
-            ((uint8_t *)destination)[i] = ((uint8_t *)source)[i];
+        size_t i = 0;
+
+#ifdef STD2_SIMD
+        for (; i + 32 <= size; i += 32)
+        {
+            __m256i source_vec = _mm256_loadu_si256((__m256i_u*) (((uint8_t*) source)[i]));
+            _mm256_storeu_si256((__m256i_u*) (((uint8_t*) destination)[i]), source_vec);
+        }
+
+        for (; i + 16 <= size; i += 16)
+        {
+            __m128i source_vec = _mm_loadu_si128((__m128i_u*) (((uint8_t*) source)[i]));
+            _mm_storeu_si128((__m128i_u*) (((uint8_t*) destination)[i]), source_vec);
+        }
+#endif
+
+        for (; i < size; i++)
+        {
+            ((uint8_t*) destination)[i] = ((uint8_t*) source)[i];
+        }
     }
 }
