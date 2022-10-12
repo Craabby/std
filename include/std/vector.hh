5aff2fb8974ed79f7961579f13620f9a92a776f4
Craabby <69945662+Craabby@users.noreply.github.com>
2022-10-11 20:47:59 -0400

    Update vector.hh

--- include/std/vector.hh
+++ include/std/vector.hh
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <cstddef>
+#include <cstdlib>
+
+#include <std/cstddef.hh>
 #include <std/memory.hh>
 #include <std/cassert.hh>
 #include <std/move.hh>
@@ -13,58 +15,57 @@ namespace std2
         static_assert(is_same_v<T, remove_cv_t<T>>, "T cannot be const or volatile");
         size_t m_Size = 0;
         size_t m_Capacity = 1;
-        T *m_Data = (T *)::operator new(sizeof(T));
+        T *m_Data = (T *)::malloc(sizeof(T));
 
         inline void Grow()
         {
-            SetCapacity(m_Capacity * 3); // will be optimized to (m_Capacity << 1) + m_Capacity
+            SetCapacity(m_Capacity << 1);
         }
 
         inline void SetCapacity(size_t size)
         {
-            T *newData = (T *)::operator new(size * sizeof(T));
+            T *newData = (T *)::malloc(size * sizeof(T));
             for (size_t i = 0; i < m_Size; i++)
                 newData[i] = Move(m_Data[i]);
-            ::operator delete(m_Data, m_Capacity * sizeof(T));
+            ::free(m_Data);
             m_Data = newData;
             m_Capacity = size;
         }
 
     public:
-        using iterator = T *;
-        using const_iterator = T const *;
-
         inline Vector() = default;
         inline Vector(Vector const &other)
         {
-            *this = other;
-        }
+            SetCapacity(other.m_Capacity);
+            for (size_t i = 0; i < other.m_Size; i++)
+                m_Data[i] = (other.m_Data[i]);
+        };
         inline Vector(Vector &&other)
         {
-            *this = Move(other);
-        }
-        // clone if it is number, otherwise const & it
-        inline Vector(size_t size, typename Conditional<is_number_v<T>, T, T const &>::type value = T())
+            m_Capacity = other.m_Capacity;
+            m_Data = other.m_Data;
+            m_Size = other.m_Size;
+            other.m_Data = nullptr;
+            other.m_Capacity = 0;
+            other.m_Size = 0;
+        };
+        inline Vector(size_t size, T const &value = T{})
         {
-            m_Data = (T *)::operator new(size * sizeof(T));
+            m_Data = (T *)::malloc(size * sizeof(T));
             m_Capacity = size;
             m_Size = size;
             for (size_t i = 0; i < m_Size; i++)
                 m_Data[i] = value;
         }
-
         inline ~Vector()
         {
-            // should not use delete[] because m_Data was not allocated using new[]
+            // cannot use delete[] because m_Data was not allocated using new[]
             for (size_t i = 0; i < m_Size; i++)
                 m_Data[i].~T();
-            ::operator delete(m_Data, m_Capacity * sizeof(T));
+            ::free(m_Data);
         }
-        inline Vector<T> &operator=(Vector<T> &&other)
+        inline Vector<T> operator=(Vector<T> &&other)
         {
-#ifdef STD2_VECTOR_SAFE
-            Assert(this != &other);
-#endif
             m_Capacity = other.m_Capacity;
             m_Data = other.m_Data;
             m_Size = other.m_Size;
@@ -72,24 +73,6 @@ namespace std2
             other.m_Capacity = 0;
             other.m_Size = 0;
             return *this;
-        }
-
-        inline Vector<T> &operator=(Vector<T> const &other)
-        {
-#ifdef STD2_VECTOR_SAFE
-            Assert(this != &other);
-#endif
-            SetCapacity(other.m_Capacity);
-            for (size_t i = 0; i < other.m_Size; i++)
-                m_Data[i] = (other.m_Data[i]);
-            return *this;
-        }
-
-        inline void Clear()
-        {
-            for (size_t i = 0; i < m_Size; i++)
-                m_Data[i].~T();
-            m_Size = 0;
         }
 
         inline void Reserve(size_t size)
@@ -100,7 +83,7 @@ namespace std2
                 SetCapacity(size);
         }
 
-        inline void Resize(size_t size, typename Conditional<is_number_v<T>, T, T const &>::type v = T())
+        inline void Resize(size_t size, const T &v = T())
         {
             Reserve(size);
             m_Size = size;
@@ -108,50 +91,37 @@ namespace std2
                 m_Data[i] = v;
         }
 
-        inline iterator begin() { return m_Data; }
-        inline iterator end() { return m_Data + m_Size; }
-        inline const_iterator cbegin() const { return m_Data; }
-        inline const_iterator cend() const { return m_Data + m_Size; }
+        inline T *begin() { return m_Data; }
+        inline T *end() { return m_Data + m_Size; }
+        inline T const *cbegin() const { return m_Data; }
+        inline T const *cend() const { return m_Data + m_Size; }
 
-        inline T &Front()
-        {
-#ifdef STD2_VECTOR_SAFE
-            Assert(m_Size);
-#endif
-            return *m_Data;
-        }
-        inline T const &Front() const
-        {
-#ifdef STD2_VECTOR_SAFE
-            Assert(m_Size);
-#endif
-            return *m_Data;
-        }
-        inline T &Back() { return m_Data[m_Size - 1]; }
-        inline T const &Back() const { return m_Data[m_Size - 1]; }
-
-        inline T &Push(typename Conditional<is_number_v<T>, T, T const &>::type value)
+        inline T const &Push(T const &value)
         {
             if (m_Size == m_Capacity)
                 Grow();
             m_Data[m_Size] = value;
             m_Size++;
 
-            return Back();
+            return value;
         }
 
-        inline T &Push(T &&x)
-        {
-            // call move constructor
-            Emplace(Move(x));
-        }
-
-        template <typename... Arguments>
-        inline T &Emplace(Arguments const &...arguments)
+        inline T &Push(T &&value)
         {
             if (m_Size == m_Capacity)
                 Grow();
-            new (&m_Data[m_Size]) T(arguments...);
+            new (&m_Data[m_Size]) T(Move(value));
+            m_Size++;
+
+            return value;
+        }
+
+        template <typename... Arguments>
+        inline T &Emplace(Arguments &&...arguments)
+        {
+            if (m_Size == m_Capacity)
+                Grow();
+            new (&m_Data[m_Size]) T(Forward<Arguments>(arguments)...);
             m_Size++;
             return m_Data[m_Size - 1];
         }
@@ -184,48 +154,19 @@ namespace std2
             Assert(index < m_Size);
 #endif
             m_Size--;
-            m_Data[index].~T();
             if constexpr (fast)
                 new (m_Data + index - 1) T(Move(m_Data[m_Size]));
             else
                 for (size_t i = index; i < m_Size; i++)
                     m_Data[i - 1] = Move(m_Data[i]);
+            m_Data[index].~T();
         }
 
-        inline void Pop()
-        {
-            m_Size--;
-            m_Data[m_Size].~T();
-        }
-
-        inline T &operator[](size_t i)
-        {
-            return At(i);
-        }
-
-        inline T const &operator[](size_t i) const
-        {
-            return At(i);
-        }
-
-        inline size_t Capacity() const
-        {
-            return m_Capacity;
-        }
-
-        inline size_t Size() const
-        {
-            return m_Size;
-        }
-
-        inline T *Data()
-        {
-            return m_Data;
-        }
-
-        inline T const *Data() const
-        {
-            return m_Data;
-        }
+        inline T &operator[](size_t i) { return At(i); }
+        inline T const &operator[](size_t i) const { return At(i); }
+        inline size_t Capacity() const { return m_Capacity; }
+        inline size_t Size() const { return m_Size; }
+        inline T *Data() { return m_Data; }
+        inline T const *Data() const { return m_Data; }
     };
 }
